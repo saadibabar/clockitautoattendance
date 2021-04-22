993df0c9b377e34cf2a3fd2e07f3d3a358cc6398
574r570rm <saadibabar@gmail.com>
2021-04-22 15:08:21 +0500

    update autologin.py
    checkin process completed.

--- autologin.py
+++ autologin.py
@@ -1,17 +1,55 @@
+import time
+
 from selenium import webdriver
+
 import yaml
 
+
+
 conf = yaml.load(open('logindata.yml'))
+
 myKtmEmail = conf['ktm_user']['email']
+
 myKtmPassword = conf['ktm_user']['password']
+
+
 
 driver = webdriver.Chrome()
 
+
+
 def login(url,usernameId, username, passwordId, password, submit_buttonId):
+
    driver.get(url)
+
    driver.find_element_by_class_name(usernameId).send_keys(username)
+
    driver.find_element_by_class_name(passwordId).send_keys(password)
+
    driver.find_element_by_class_name(submit_buttonId).click()
+
    
 
+def checkin(url,checkin_btn):
+
+	driver.get(url)
+
+	driver.find_element_by_class_name(capture_btn).click()
+
+
+
+
+
+
+
 login("https://clockit.me/index.php/auth/login", "login-userpin", myKtmEmail, "login-password", myKtmPassword, "login-btn")
+
+time.sleep(3)
+
+#checkin("https://clockit.me/index.php/auth/dashboard","capture_btn")
+
+driver.find_element_by_link_text ("Checkin"). click ()
+
+time.sleep(3)
+
+driver.find_element_by_id("snap").click()
